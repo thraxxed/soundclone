268d5f69493bcac637c75391591f3af525bb0472
seth <sealittl@ucsc.edu>
2018-01-31 22:11:32 -0800

    even more styling

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -12,7 +12,7 @@ class Splash extends React.Component {
         <div className="hero-flex">
           <div className="hero-buttons">
             <Link id="hero-signin-btn" className="btn" to="/login"><span className="button-text">Sign In</span></Link>
-            <Link id="signup-btn" className="btn" to="/signup">Create Account</Link>
+            <Link id="hero-signup-btn" className="btn" to="/signup">Create Account</Link>
           </div>
           <br></br>
           <br></br>
@@ -21,7 +21,11 @@ class Splash extends React.Component {
           <br></br>
           <br></br>
           <br></br>
-          <p>Discover, stream, and share a constantly expanding mix of music<br></br> from emerging and major artists around the world</p>
+          <p>
+            Discover, stream, and share a constantly expanding mix of music
+            <br></br>
+            from emerging and major artists around the world.
+          </p>
           <br></br>
           <Link id="hero-bottom-signup-btn" className="btn" to="/signup">Sign up for free</Link>
         </div>
