d0129c0ba892cc72408661093bbc7eed34adb4d0
seth <sealittl@ucsc.edu>
2018-02-05 17:42:01 -0800

    comments reducer

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,24 @@
+import merge from 'lodash/merge';
+
+import { RECEIVE_ALL_COMMENTS, RECEIVE_COMMENT, REMOVE_COMMENT } from '../actions/comment.js';
+
+const commentsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+  switch (action.type) {
+    case RECEIVE_ALL_COMMENTS:
+      return merge({}, state, action.payload.comments);
+    case RECEIVE_COMMENT:
+      newState = merge({}, state);
+      newState[action.comment.id] = action.comment;
+      return newState;
+    case REMOVE_COMMENT:
+      newState = merge({}, state);
+      delete newState[action.commentId];
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default commentsReducer;
